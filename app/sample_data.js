d6f7afc04ae958443e30f6a169249ef9410b31f8
Agusti Bau <agustibau@gmail.com>
2019-11-21 20:28:50 +0000

    basic questionin feature

---
+++ app/sample_data.js
@@ -0,0 +1,46 @@
+
+
+
+export default 
+[
+    [
+        "How are you feeling about your recent hip surgergy?",
+        "Are you uncomfortably bleeding?",
+        "Have you changed your wound dressing because it was soaked?",
+        "How long did it take to get soaked?",
+        "Can you check if you have fever?",
+        "What is the temperature?",
+        "How long have you been at that temperature for? An estimate is enough",
+        "Are you currently in pain?",
+        "Where?",
+        "How intense is it? Just explain",
+        "Please show the dressing to the camera"
+    ],
+    [
+        "How are you feeling about your recovery process? ",
+        "Are you uncomfortably bleeding?",
+        "Have you changed your wound dressing because it was soaked?",
+        "How long did it take to get soaked?",
+        "Can you check if you have fever?",
+        "What is the temperature?",
+        "How long have you been at that temperature for? An estimate is enough",
+        "Are you currently in pain?",
+        "Where?",
+        "How intense is it? Just explain, please",
+        "Is there what you would consider too much redness around the wound?",
+        "Is there pus around the wound?",
+        "lastly, is there discharge around the wound?",
+        "Point the camera at the wound please"
+    ],
+    [
+        "How are you feeling about your recent hip surgergy?",
+        "Have you felt nauseous recently?",
+        "Have you felt pain recently?",
+        "How are you finding your mobility routine?",
+        "Are you struggling with any of the exercises in particular?",
+        "Are you feeling any pain during the routine?",
+        "Is the pain stopping you from performing the exercice?",
+        "Do you feel like you need in person help?",
+        "Show the scar or dressing to the camera please",
+    ]
+]
